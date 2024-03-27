5da47a0cbbbc811f134506807b1eb0ab3a7e8620
Capra Audio <122894651+CapraAudio@users.noreply.github.com>
2024-03-27 17:34:30 -0400

    Customization of the Graphtool
    Initial customization of the graphtool.

--- assets/js/config_hp.js
+++ assets/js/config_hp.js
@@ -1,24 +1,26 @@
 // Configuration options
-const init_phones = ["IEF Neutral Target"],                             // Optional. Which graphs to display on initial load. Note: Share URLs will override this set
-      DIR = "data_hp/",                                // Directory where graph files are stored
+const init_phones = ["KEMAR DF for KB-500x Target", "Satyr-1"], // Optional. Which graphs to display on initial load. Note: Share URLs will override this set
+      DIR = "/data_hp/",                             // Directory where graph files are stored
       default_channels = ["L","R"],                 // Which channels to display. Avoid javascript errors if loading just one channel per phone
-      default_normalization = "dB",                 // Sets default graph normalization mode. Accepts "dB" or "Hz"
-      default_norm_db = 60,                         // Sets default dB normalization point
-      default_norm_hz = 1000,                       // Sets default Hz normalization point (500Hz is recommended by IEC)
+      default_normalization = "Hz",                 // Sets default graph normalization mode. Accepts "dB" or "Hz"
+      default_norm_db = 35,                         // Sets default dB normalization point
+      default_norm_hz = 630,                        // Sets default Hz normalization point (500Hz is recommended by IEC)
       max_channel_imbalance = 5,                    // Channel imbalance threshold to show ! in the channel selector
       alt_layout = true,                            // Toggle between classic and alt layouts
       alt_sticky_graph = true,                      // If active graphs overflows the viewport, does the graph scroll with the page or stick to the viewport?
       alt_animated = true,                          // Determines if new graphs are drawn with a 1-second animation, or appear instantly
       alt_header = true,                            // Display a configurable header at the top of the alt layout
-      alt_tutorial = true,                          // Display a configurable frequency response guide below the graph
-      site_url = '/',                               // URL of your graph "homepage"
+      alt_tutorial = false,                         // Display a configurable frequency response guide below the graph
+      alt_augment = true,                           // Display augment card in phone list, e.g. review sore, shop link
+      site_url = '',                                // URL of your graph "homepage"
       share_url = true,                             // If true, enables shareable URLs
-      watermark_text = "HarutoHiroki",              // Optional. Watermark appears behind graphs
-      watermark_image_url = "assets/images/haruto.svg",   // Optional. If image file is in same directory as config, can be just the filename
-      page_title = "HarutoHiroki",                  // Optional. Appended to the page title if share URLs are enabled
-      page_description = "View and compare frequency response graphs for headphones.",
-      accessories = true,                           // If true, displays specified HTML at the bottom of the page. Configure further below
-      externalLinksBar = true,                      // If true, displays row of pill-shaped links at the bottom of the page. Configure further below
+      preference_bounds = "assets/images/bounds.png",  // Optional. If png file is present, display bounds image
+      watermark_text = "Capra Audio",               // Optional. Watermark appears behind graphs
+      watermark_image_url = "assets/images/Capra-Audio-Logo.png",// Optional. If image file is in same directory as config, can be just the filename
+      page_title = "Capra Audio",                   // Optional. Appended to the page title if share URLs are enabled
+      page_description = "Capra Audio Squigtool or smth.",
+      accessories = false,                          // If true, displays specified HTML at the bottom of the page. Configure further below
+      externalLinksBar = false,                     // If true, displays row of pill-shaped links at the bottom of the page. Configure further below
       expandable = false,                           // Enables button to expand iframe over the top of the parent page
       expandableOnly = false,                       // Prevents iframe interactions unless the user has expanded it. Accepts "true" or "false" OR a pixel value; if pixel value, that is used as the maximum width at which expandableOnly is used
       headerHeight = '0px',                         // Optional. If expandable=true, determines how much space to leave for the parent page header
@@ -31,29 +33,24 @@ const init_phones = ["IEF Neutral Target"],                             // Optio
       extraEnabled = true,                          // Enable extra features
       extraUploadEnabled = true,                    // Enable upload function
       extraEQEnabled = true,                        // Enable parametic eq function
-      extraEQBands = 10,                            // Default EQ bands available
+      extraEQBands = 3,                             // Default EQ bands available
       extraEQBandsMax = 20,                         // Max EQ bands available
       num_samples = 5,                              // Number of samples to average for smoothing
-      scale_smoothing = 0.2;                        // Smoothing factor for scale transitions
-      
+      scale_smoothing = 0.2,                        // Smoothing factor for scale transitions
+      PHONE_BOOK = "phone_book_hp.json",            // Path to phone book JSON file
+      REWenabled = true,                            // Enable REW import function
+      default_bass_shelf = 0,                       // Default Custom DF bass shelf value
+      default_tilt = -1.0,                          // Default Custom DF tilt value
+      default_ear = 0,                              // Default Custom DF ear gain value
+      default_treble = 0,                           // Default Custom DF treble gain value
+      default_DF_name = "KEMAR DF for KB-500x",     // Default RAW DF name
+      dfBaseline = false,                           // If true, DF is used as baseline when custom df tilt is on
+      tiltableTargets = ["KEMAR DF for KB-500x"];   // Targets that are allowed to be tilted
 
 // Specify which targets to display
 const targets = [
-    { type:"Neutral",    files:["KEMAR DF", "IEF Neutral"] },
-    { type:"Community",   files:["Listener Tilt 711"] },
-    { type:"Preference", files:["Harman Combined", "Harman 2018 OE", "Harman 2015 OE", "Harman 2013 OE"] }
+    { type:"",    files:["KEMAR DF for KB-500x"] },
 ];
-
-// Haruto's Addons
-const  preference_bounds = "assets/images/bounds.png", // Preference bounds image
-       PHONE_BOOK = "phone_book_hp.json",            // Phone book file path & name
-       default_DF_name = "KEMAR DF",                 // Default RAW DF name
-       dfBaseline = false,                           // If true, DF is used as baseline when custom df tilt is on
-       default_bass_shelf = 8,                      // Default Custom DF bass shelf value
-       default_tilt = -0.8,                         // Default Custom DF tilt value
-       default_ear = 0,                             // Default Custom DF ear gain value
-       default_treble = 0,                          // Default Custom DF treble gain value
-       tiltableTargets = ["KEMAR DF"];               // Targets that are allowed to be tilted
 
 
 
@@ -90,7 +87,7 @@ function watermark(svg) {
         .attr("opacity",0.2)
         .append("text")
         .attrs({x:765, y:314, "font-size":10, "text-anchor":"end", "class":"site_name"})
-        .text("graphtool.harutohiroki.com");
+        .text("graphtool.tedthepraimortis.com");
 }
 
 
@@ -221,29 +218,21 @@ setupGraphAnalytics();
 
 
 // If alt_header is enabled, these are the items added to the header
-let headerLogoText = "HarutoHiroki",
-    headerLogoImgUrl = "assets/images/haruto.svg",
+let headerLogoText = "Capra-Audio",
+    headerLogoImgUrl = "assets/images/capyay.svg",
     headerLinks = [
     {
-        name: "Home",
-        url: "https://harutohiroki.com"
+        name: "IEMs",
+        url: "index.html"
     },
     {
-        name: "Ranking",
-        url: "https://docs.google.com/spreadsheets/d/1DZTac1BxCLdmS2J4DDQyvKSVUZGnNhz2r86qMGcs_Jo/edit?pli=1#gid=330037169"
+        name: "Headphones",
+        url: "headphones.html"
     },
     {
-        name: "Discord",
-        url: "https://discord.harutohiroki.com"
+        name: "Earbuds",
+        url: "earbuds.html"
     },
-    {
-        name: "Donate",
-        url: "https://www.paypal.me/harutohirokiUS"
-    },
-//  {
-//      name: "GitHub",
-//      url: "https://github.com/HarutoHiroki"
-//  },
 ];
 
 // Source: https://www.teachmeaudio.com/mixing/techniques/audio-spectrum
