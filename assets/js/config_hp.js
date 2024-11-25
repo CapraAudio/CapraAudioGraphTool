526898c46b3ea649103a785dd19e20a5b555a0db
Capra Audio <122894651+CapraAudio@users.noreply.github.com>
2024-11-24 21:40:30 -0500

    Update config_hp.js

--- assets/js/config_hp.js
+++ assets/js/config_hp.js
@@ -39,7 +39,7 @@ const init_phones = ["KEMAR DF KB50xx Target", "Ouroboros"], // Optional. Which 
 
 // Specify which targets to display
 const targets = [
-    { type:"HRTF",    files:["KEMAR DF KB50xx"] },
+    { type:"HRTF",    files:["KEMAR DF (KB50xx) Target"] },
 ];
 
 // Haruto's Addons
